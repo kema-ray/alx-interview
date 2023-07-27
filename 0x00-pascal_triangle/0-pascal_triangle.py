8248dcc283812bd5d71e295cc6a4f86bf9d02f2b
kema-ray <rachelkemuma99@gmail.com>
2023-07-27 08:59:09 +0300

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+"""
+Module for Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of integers representing the Pascal's triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize with the first row containing just 1
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]  # First element of each row is always 1
+
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])  # Calculate middle elements of the row
+
+        new_row.append(1)  # Last element of each row is always 1
+        triangle.append(new_row)
+
+    return triangle
