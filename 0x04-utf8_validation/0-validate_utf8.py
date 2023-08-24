42b1b8cd2c269d75b067565a055f3e49892397ff
kema-ray <rachelkemuma99@gmail.com>
2023-08-24 14:39:57 +0300

    UTF-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -21,18 +21,18 @@ def validUTF8(data):
     for num in data:
         """Checks if the first bit is a 0"""
         if number_of_bytes == 0:
-            num  = num  & mask
-            if (num >> 5) == 0b110:
+            num = num & mask
+            if(num >> 5) == 0b110:
                 number_of_bytes = 1
-            elif (num  >> 4) == 0b1110:
+            elif(num >> 4) == 0b1110:
                 number_of_bytes = 2
-            elif (num  >> 3) == 0b11110:
+            elif(num >> 3) == 0b11110:
                 number_of_bytes = 3
-            elif (num  >> 7):
+            elif(num >> 7):
                 return False
         else:
-            if (num  >> 6) != 0b10:
+            if(num >> 6) != 0b10:
                 return False
             number_of_bytes -= 1
 
-    return number_of_bytes == 0 
+    return number_of_bytes == 0
